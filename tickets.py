d451da170ba22095a4dec6f2fd68f7c9196f4ca1
Qoba <reshetovandrew1@gmail.com>
2022-04-20 20:21:52 +0300

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -1,15 +1,23 @@
-tickets = int(input("Введите количество покупаемых билетов: "))
-total = 0
-for i in range(tickets):
-    age = int(input(f'Возраст {i+1} человека: '))
-    if age < 18:
-        total += 0
-    elif 18 <= age < 25:
-        total += 990
-    else:
-        total += 1390
-if tickets > 3:
-    discount = 0.9
+try:
+    tickets = int(input("Введите количество покупаемых билетов: "))
+except ValueError:
+    print('Вы ввели неправильное число')
 else:
-    discount = 1
-print("Сумма к оплате: ", total * discount)
+    total = 0
+    for i in range(tickets):
+        try:
+            age = int(input(f'Возраст {i+1} человека: '))
+        except ValueError:
+            print('Вы ввели неправильное число')
+        else:    
+            if age < 18:
+                total += 0
+            elif 18 <= age < 25:
+                total += 990
+            else:
+                total += 1390
+    if tickets > 3:
+        discount = 0.9
+    else:
+        discount = 1
+    print("Сумма к оплате: ", total * discount)
